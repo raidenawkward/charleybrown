41bf461ff4c93cd03ec7b345a2bc1c52dfc7ca38
Raiden Awkward <raiden.ht@gmail.com>
2012-03-19 12:33:03 +0800

    CB ui updated
    modified: SingleMenuView updated;
    modified: AndroidManifest: program launches with landscape status;

--- CB/src/com/android/cb/view/SingleMenuView.java
+++ CB/src/com/android/cb/view/SingleMenuView.java
@@ -12,96 +12,80 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.KeyEvent;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnGestureListener;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * @author raiden
  *
  * @Description single menu view
  */
-public class SingleMenuView extends SurfaceView implements SurfaceHolder.Callback {
+public class SingleMenuView extends SurfaceView implements
+	SurfaceHolder.Callback,OnGestureListener, android.view.View.OnTouchListener {
 
 	private SurfaceHolder mSurfaceHolder;
 	private TestMovingRunnable mTestMovingRunnable;
 	private Thread mTestDrawingTrhead;
+	private GestureDetector mGuestureDetctor;
 
 	private void initSurfaceHolder() {
 		mSurfaceHolder = getHolder();
 		mSurfaceHolder.addCallback(this);
+
+		mGuestureDetctor = new GestureDetector(this);
+//		 BitmapFactory
 	}
 
 	public SingleMenuView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
 		initSurfaceHolder();
 	}
 
 	public SingleMenuView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
 		initSurfaceHolder();
 	}
 
 	public SingleMenuView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
 		initSurfaceHolder();
 	}
 
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		// TODO Auto-generated method stub
-		return mTestMovingRunnable.onKeyDown(keyCode, event);
-	}
-
-	@Override
-	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		// TODO Auto-generated method stub
-		return mTestMovingRunnable.onKeyUp(keyCode, event);
-	}
-
-	@Override
-	protected void onDraw(Canvas canvas) {
-		// TODO Auto-generated method stub
-		super.onDraw(canvas);
-	}
-
 	public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	public void surfaceCreated(SurfaceHolder arg0) {
-		// TODO Auto-generated method stub
-//		new Thread(new TestDrawingThread()).start();
 		mTestMovingRunnable = new TestMovingRunnable();
 		mTestDrawingTrhead = new Thread(mTestMovingRunnable);
 		mTestDrawingTrhead.start();
 	}
 
 	public void surfaceDestroyed(SurfaceHolder arg0) {
-		// TODO Auto-generated method stub
-//		mTestDrawingTrhead.stop();
-	}
+		while (true) {
+			try {
+				mTestDrawingTrhead.join();
+				break ;
+			}
+			catch(Exception ex){
 
-	class TestDrawingRunnable implements Runnable {  
-        public void run() {
-            Canvas canvas = mSurfaceHolder.lockCanvas(null);
-            Paint mPaint = new Paint();
-            mPaint.setColor(Color.BLUE);
-            canvas.drawRect(new RectF(40,60,80,80), mPaint);
-            mSurfaceHolder.unlockCanvasAndPost(canvas);
-        }
-    }
+			}
+		}
+	}
 
 	class TestMovingRunnable implements Runnable {
 		private int mX = 40;
 		private int mY = 60;
 		private int mW = 80;
 		private int mH = 80;
+
+		private int mSignX = 1;
+		private int mSignY = 1;
+		private final int MOVESTEP = 2;
 
 		private RectF mRect;
 		private Paint mPaint;
@@ -116,63 +100,65 @@ public class SingleMenuView extends SurfaceView implements SurfaceHolder.Callbac
 				synchronized (mSurfaceHolder) {
 					try {
 						Canvas canvas = mSurfaceHolder.lockCanvas();
-						mX += 5;
-						mY += 5;
-						mRect.set(mX, mY, mX + mW, mY + mH);
 						canvas.drawColor(Color.BLACK);
+
+						mX += MOVESTEP * mSignX;
+						mY += MOVESTEP * mSignY;
+						if (mX < 0 || mX + mW > SingleMenuView.this.getWidth())
+							mSignX *= -1;
+						if (mY < 0 || mY + mH > SingleMenuView.this.getHeight())
+							mSignY *= -1;
+
+						mRect.set(mX, mY, mX + mW, mY + mH);
+						mPaint.setColor(Color.BLUE);
 						canvas.drawRect(mRect, mPaint);
 						onDraw(canvas);
 						mSurfaceHolder.unlockCanvasAndPost(canvas);
-						Thread.sleep(250);
-						String msg = new String();
-						msg += "# painting : " + mX + "," + mY + "," + mW + "," + mH;
-						Log.d("************************* ", msg);
+						Thread.sleep(1);
+
 					} catch (InterruptedException e) {
 						e.printStackTrace();
 					}
-				}
-			}
+				} // sync
+			} // while
 		}
 
-		private final int mMoveStep = 30;
+	}
 
-		public boolean onKeyDown(int keyCode, KeyEvent event) {
-			synchronized (mSurfaceHolder) {
-				Log.d("************************* keyCode", String.valueOf(keyCode));
-				switch (keyCode) {
-				case KeyEvent.KEYCODE_DPAD_UP:
-				case KeyEvent.KEYCODE_K:
-					mY -= mMoveStep;
-					if (mY < 0)
-						mY = 0;
-					break;
-				case KeyEvent.KEYCODE_DPAD_DOWN:
-				case KeyEvent.KEYCODE_J:
-					mY += mMoveStep;
-					break;
-				case KeyEvent.KEYCODE_DPAD_LEFT:
-				case KeyEvent.KEYCODE_H:
-					mX -= mMoveStep;
-					if (mX < 0)
-						mX = 0;
-					break;
-				case KeyEvent.KEYCODE_DPAD_RIGHT:
-				case KeyEvent.KEYCODE_L:
-					mX += mMoveStep;
-					break;
-				default:
-					return false;
-				}
-			} // synchronized
+	public boolean onDown(MotionEvent e) {
+		Toast.makeText(this.getContext(), new String("down"), 0).show();
+		return true;
+	}
 
-			return true;
-		}
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+			float velocityY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
-		public boolean onKeyUp(int keyCode, KeyEvent event) {
-			return true;
-		}
+	public void onLongPress(MotionEvent e) {
+		// TODO Auto-generated method stub
 
+	}
 
+	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
+			float distanceY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void onShowPress(MotionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean onSingleTapUp(MotionEvent e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean onTouch(View v, MotionEvent event) {
+		return this.mGuestureDetctor.onTouchEvent(event);
 	}
 
 }
