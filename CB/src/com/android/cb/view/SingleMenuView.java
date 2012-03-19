b2e82f664b2fd3932a74c96a2b097b0417daf8a1
raiden <raiden.ht@gmail.com>
2012-03-19 19:32:03 +0800

    CB ui updated
    modified: draw methods updated for SingleMenuView;

--- CB/src/com/android/cb/view/SingleMenuView.java
+++ CB/src/com/android/cb/view/SingleMenuView.java
@@ -6,7 +6,19 @@
  */
 package com.android.cb.view;
 
+import java.util.ArrayList;
+
+import com.android.cb.R;
+
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.GestureDetector.OnGestureListener;
@@ -21,6 +33,18 @@ import android.widget.Toast;
  *
  * @Description single menu view
  */
+/**
+ * @author huangtao
+ *
+ */
+/**
+ * @author huangtao
+ *
+ */
+/**
+ * @author huangtao
+ *
+ */
 public class SingleMenuView extends SurfaceView implements
 	SurfaceHolder.Callback, OnGestureListener, android.view.View.OnTouchListener, Runnable {
 
@@ -28,15 +52,37 @@ public class SingleMenuView extends SurfaceView implements
 //	private TestMovingRunnable mTestMovingRunnable;
 //	private Thread mTestDrawingTrhead;
 	private GestureDetector mGuestureDetctor;
+	private ArrayList<Bitmap> mPictures;
+	private float mSplitLineX = 0;
 
 	private void initSurfaceHolder() {
+		mSplitLineX = 0;
 		mSurfaceHolder = getHolder();
 		mSurfaceHolder.addCallback(this);
 
 		mGuestureDetctor = new GestureDetector(this);
-//		 BitmapFactory
+
+
+		Bitmap map1 = BitmapFactory.decodeResource(getResources(), R.drawable.img0001);
+		Bitmap map2 = BitmapFactory.decodeResource(getResources(), R.drawable.img0001);
+		mPictures = new ArrayList<Bitmap>();
+
+		mPictures.add(scaleBitmapToFixView(map1));
+		mPictures.add(scaleBitmapToFixView(map2));
 	}
 
+	private Bitmap scaleBitmapToFixView(Bitmap bitmap) {
+		if (bitmap == null)
+			return null;
+
+		float scaleW = getWidth() / bitmap.getWidth();
+		float scaleH = getHeight() / bitmap.getHeight();
+
+		Matrix matrix = new Matrix();
+		matrix.postScale(scaleW, scaleH);
+
+		return Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+	}
 	public SingleMenuView(Context context) {
 		super(context);
 		initSurfaceHolder();
@@ -59,6 +105,8 @@ public class SingleMenuView extends SurfaceView implements
 //		mTestMovingRunnable = new TestMovingRunnable();
 //		mTestDrawingTrhead = new Thread(mTestMovingRunnable);
 //		mTestDrawingTrhead.start();
+		mSplitLineX = 0;
+		draw2SpitedBitmap(mSplitLineX,mPictures.get(0),mPictures.get(1));
 	}
 
 	public void surfaceDestroyed(SurfaceHolder arg0) {
@@ -137,11 +185,12 @@ public class SingleMenuView extends SurfaceView implements
 
 	public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX,
 			float distanceY) {
+		mSplitLineX = e2.getX();
+		draw2SpitedBitmap(mSplitLineX,mPictures.get(0),mPictures.get(1));
 		return false;
 	}
 
 	public void onShowPress(MotionEvent e) {
-
 	}
 
 	public boolean onSingleTapUp(MotionEvent e) {
@@ -163,5 +212,35 @@ public class SingleMenuView extends SurfaceView implements
 		return this.mGuestureDetctor.onTouchEvent(event);
 	}
 
+	protected void draw2SpitedBitmap(float offset, Bitmap pl, Bitmap pr) {
+		synchronized (mSurfaceHolder) {
+			if (pl == null || pr == null)
+				return;
+			if (offset < 0 || offset > this.getWidth())
+				return;
+
+			Toast.makeText(this.getContext(), new String("p: " + pl.getWidth() + "x" + pl.getHeight() + " s: " + this.getWidth() + "x" + this.getHeight()), 0).show();
+			Canvas canvas = mSurfaceHolder.lockCanvas();
+			canvas.drawColor(Color.BLACK);
+
+			Paint paint = new Paint();
+
+			int off = (int) offset;
+
+			canvas.drawBitmap(pl,
+					new Rect(pl.getWidth() - off, 0, pl.getWidth(), pl.getHeight()),
+					new RectF(0, 0, off, this.getHeight()),
+					paint);
+
+			canvas.drawBitmap(pr,
+					new Rect(0, 0, this.getWidth() - off, pr.getHeight()),
+					new RectF(off, 0, this.getWidth(), this.getHeight()),
+					paint);
+
+			onDraw(canvas);
+
+			mSurfaceHolder.unlockCanvasAndPost(canvas);
+		}
+	}
 
 }
