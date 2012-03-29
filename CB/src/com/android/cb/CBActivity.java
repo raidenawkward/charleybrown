d04bd5c3e31f3fa34dd2bf3fb5b7e04b20b1c4b3
Raiden Awkward <raiden.ht@gmail.com>
2012-03-29 22:39:00 +0800

    CB: GridViewActivity added
    added: main_grid_menu_view.xml: layout for GridViewActivity;
    added: GridViewActivity: activity with buttons group and grid menu view;
    modified: CBActivity: functions for testing removed;
    modified: main.xml: start button added;

--- CB/src/com/android/cb/CBActivity.java
+++ CB/src/com/android/cb/CBActivity.java
@@ -1,29 +1,18 @@
 package com.android.cb;
 
-import com.android.cb.source.CBPathWalker;
-import com.android.cb.support.CBDish;
-import com.android.cb.support.CBId;
-import com.android.cb.support.CBMenuEngine;
-import com.android.cb.support.CBMenuItem;
-import com.android.cb.support.CBMenuItemsSet;
-import com.android.cb.support.CBOrder;
-import com.android.cb.view.CBButton;
-import com.android.cb.view.CBButtonsGroup;
-import com.android.cb.view.GridMenuView;
-import com.android.cb.view.SingleMenuView;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
+import android.widget.Button;
 
 public class CBActivity extends Activity {
-	LinearLayout mLayoutMain;
-	CBMenuEngine mMenuEngine;
-	SingleMenuView mSingleView;
-	GridMenuView mGridView;
+//	LinearLayout mLayoutMain;
+//	CBMenuEngine mMenuEngine;
+//	SingleMenuView mSingleView;
+//	GridMenuView mGridView;
 
     /** Called when the activity is first created. */
     @Override
@@ -34,101 +23,101 @@ public class CBActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		testingMenuEnginePrepare();
+		Button buttonStart = (Button) this.findViewById(R.id.button_start);
+		buttonStart.setOnClickListener(new Button.OnClickListener() {
 
-		mLayoutMain = (LinearLayout) findViewById(R.id.mainview);
+			public void onClick(View arg0) {
+				Intent intent = new Intent();
+				intent.setClass(CBActivity.this, GridViewActivity.class);
+				CBActivity.this.startActivity(intent);
+			}
+
+		});
+
+//		mLayoutMain = (LinearLayout) findViewById(R.id.mainview);
 
 		// testing for singleview
 //		mSingleView = new SingleMenuView(this);
 //		mSingleView.setMenuEngine(mMenuEngine);
 //		mLayoutMain.addView(mSingleView);
-
-		// testing for gridview
-//		mGridView = new GridMenuView(this);
-//		mGridView.setMenuItemSet(mMenuEngine.getMenuSet());
-//		mGridView.setMenuItemSet(mMenuEngine.getMenuItemsSetWithTag(sCurrentTag));
-//		mLayoutMain.addView(mGridView);
-
-		// testing for buttongroup
-		testButtonGroup();
     }
 
-    private void testingMenuEnginePrepare() {
-		CBMenuItemsSet set = new CBMenuItemsSet();
-		for (int i = 1; i <= 242; ++i) {
-			CBId id = new CBId();
-			id.setId(String.valueOf(i));
-
-			CBDish dish = new CBDish();
-			String image = "/sdcard/image/img" + String.valueOf(i) + ".jpg";
-			Log.d("## ", "loading: " +image);
-			dish.setPicture(image);
-			dish.setThumb(image);
-			dish.setId(id);
-			dish.setName("img" + String.valueOf(i));
-			if (i % 2 == 0) {
-				dish.addTag("even");
-			} else {
-				dish.addTag("odd");
-			}
-
-			CBMenuItem item = new CBMenuItem();
-			item.setIndex(i - 1);
-			item.setDish(dish);
-			set.add(item);
-		}
-
-		CBOrder order = new CBOrder();
-		mMenuEngine = new CBMenuEngine();
-		mMenuEngine.setMenuSet(set);
-		mMenuEngine.setOrder(order);
-	}
-
-    private class TestPathWalker implements CBPathWalker.Callback {
-
-		public boolean onDirDetected(String dir, int depth) {
-			Log.d("dir found", dir + ", depth: " + depth);
-			return true;
-		}
-
-		public boolean onFileDetected(String file, int depth) {
-			Log.d("file found", file + ", depth: " + depth);
-			return true;
-		}
-
-    }
-
-	@SuppressWarnings("unused")
-	private void testPathWalker() {
-		CBPathWalker walker = new CBPathWalker(new TestPathWalker());
-		walker.setRoot("/sdcard");
-		walker.go();
-	}
-
-	private void testButtonGroup() {
-		CBButtonsGroup buttonGroup = (CBButtonsGroup) this.findViewById(R.id.buttonGroup);
-
-		for (int i = 0; i < 6; ++i) {
-			CBButton button = new CBButton(this, R.drawable.button_orange, R.drawable.button_gray);
-			button.setText("this is button " + i + "with a long name");
-			buttonGroup.addButton(button);
-		}
-	}
-
-	private static String sCurrentTag = "odd";
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		switch(keyCode) {
-		case KeyEvent.KEYCODE_MENU:
-			if (sCurrentTag == "odd")
-				sCurrentTag = "even";
-			else
-				sCurrentTag = "odd";
-			mGridView.setMenuItemSet(mMenuEngine.getMenuItemsSetWithTag(sCurrentTag));
-			break;
-		}
-
-		return super.onKeyDown(keyCode, event);
-	}
+//    private void testingMenuEnginePrepare() {
+//		CBMenuItemsSet set = new CBMenuItemsSet();
+//		for (int i = 1; i <= 242; ++i) {
+//			CBId id = new CBId();
+//			id.setId(String.valueOf(i));
+//
+//			CBDish dish = new CBDish();
+//			String image = "/sdcard/image/img" + String.valueOf(i) + ".jpg";
+//			Log.d("## ", "loading: " +image);
+//			dish.setPicture(image);
+//			dish.setThumb(image);
+//			dish.setId(id);
+//			dish.setName("img" + String.valueOf(i));
+//			if (i % 2 == 0) {
+//				dish.addTag("even");
+//			} else {
+//				dish.addTag("odd");
+//			}
+//
+//			CBMenuItem item = new CBMenuItem();
+//			item.setIndex(i - 1);
+//			item.setDish(dish);
+//			set.add(item);
+//		}
+//
+//		CBOrder order = new CBOrder();
+//		mMenuEngine = new CBMenuEngine();
+//		mMenuEngine.setMenuSet(set);
+//		mMenuEngine.setOrder(order);
+//	}
+//
+//    private class TestPathWalker implements CBPathWalker.Callback {
+//
+//		public boolean onDirDetected(String dir, int depth) {
+//			Log.d("dir found", dir + ", depth: " + depth);
+//			return true;
+//		}
+//
+//		public boolean onFileDetected(String file, int depth) {
+//			Log.d("file found", file + ", depth: " + depth);
+//			return true;
+//		}
+//
+//    }
+//
+//	@SuppressWarnings("unused")
+//	private void testPathWalker() {
+//		CBPathWalker walker = new CBPathWalker(new TestPathWalker());
+//		walker.setRoot("/sdcard");
+//		walker.go();
+//	}
+//
+//	private void testButtonGroup() {
+//		CBButtonsGroup buttonGroup = (CBButtonsGroup) this.findViewById(R.id.buttonsGroup);
+//
+//		for (int i = 0; i < 6; ++i) {
+//			CBButton button = new CBButton(buttonGroup.getContext(), R.drawable.button_orange, R.drawable.button_gray);
+//			button.setText("this is button " + i + "with a long name");
+//			buttonGroup.addButton(button);
+//		}
+//	}
+//
+//	private static String sCurrentTag = "odd";
+//	@Override
+//	public boolean onKeyDown(int keyCode, KeyEvent event) {
+//		switch(keyCode) {
+//		case KeyEvent.KEYCODE_MENU:
+//			if (sCurrentTag == "odd")
+//				sCurrentTag = "even";
+//			else
+//				sCurrentTag = "odd";
+//			mGridView.setMenuItemSet(mMenuEngine.getMenuItemsSetWithTag(sCurrentTag));
+//			break;
+//		}
+//
+//		return super.onKeyDown(keyCode, event);
+//	}
 
 }
---
+++ CB/src/com/android/cb/GridViewActivity.java
@@ -0,0 +1,100 @@
+/**
+ * @Title: GridViewActivity.java
+ * @Package: com.android.cb
+ * @Author: Raiden Awkward<raiden.ht@gmail.com>
+ * @Date: 2012-3-29
+ */
+package com.android.cb;
+
+import com.android.cb.support.CBDish;
+import com.android.cb.support.CBId;
+import com.android.cb.support.CBMenuEngine;
+import com.android.cb.support.CBMenuItem;
+import com.android.cb.support.CBMenuItemsSet;
+import com.android.cb.support.CBOrder;
+import com.android.cb.view.CBButton;
+import com.android.cb.view.CBButtonsGroup;
+import com.android.cb.view.GridMenuView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * @author raiden
+ *
+ * @Description grid style activity
+ */
+public class GridViewActivity extends Activity {
+
+	private CBMenuEngine mMenuEngine;
+	private GridMenuView mGridView;
+	private CBButtonsGroup mButtonsGruop;
+
+	public GridViewActivity() {
+		super();
+
+		initMenuEngine();
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+		setContentView(R.layout.main_grid_menu_view);
+
+		initButtonGroups();
+		initGridMenuView();
+
+		super.onCreate(savedInstanceState);
+	}
+
+	private void initMenuEngine() {
+		CBMenuItemsSet set = new CBMenuItemsSet();
+		for (int i = 1; i <= 242; ++i) {
+			CBId id = new CBId();
+			id.setId(String.valueOf(i));
+
+			CBDish dish = new CBDish();
+			String image = "/sdcard/image/img" + String.valueOf(i) + ".jpg";
+			dish.setPicture(image);
+			dish.setThumb(image);
+			dish.setId(id);
+			dish.setName("img" + String.valueOf(i));
+			if (i % 2 == 0) {
+				dish.addTag("even");
+			} else {
+				dish.addTag("odd");
+			}
+
+			CBMenuItem item = new CBMenuItem();
+			item.setIndex(i - 1);
+			item.setDish(dish);
+			set.add(item);
+		}
+
+		CBOrder order = new CBOrder();
+		mMenuEngine = new CBMenuEngine();
+		mMenuEngine.setMenuSet(set);
+		mMenuEngine.setOrder(order);
+	}
+
+	private void initButtonGroups() {
+		mButtonsGruop = (CBButtonsGroup) this.findViewById(R.id.buttonsGroup);
+
+		for (int i = 0; i < 6; ++i) {
+			CBButton button = new CBButton(mButtonsGruop.getContext(), R.drawable.button_orange, R.drawable.button_gray);
+			button.setText("this is button " + i + "with a long name");
+			mButtonsGruop.addButton(button);
+		}
+	}
+
+	private void initGridMenuView() {
+		mGridView = (GridMenuView) this.findViewById(R.id.gridMenuView);
+		mGridView.setMenuItemSet(mMenuEngine.getMenuSet());
+//		mGridView.setMenuItemSet(mMenuEngine.getMenuItemsSetWithTag(sCurrentTag));
+	}
+
+}
