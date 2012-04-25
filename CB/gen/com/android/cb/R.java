a892cac3a60f3b0459296735e60add6bafc0848f
Raiden Awkward <raiden.ht@gmail.com>
2012-04-25 21:40:18 +0800

    CB: loading icon added

--- CB/gen/com/android/cb/R.java
+++ CB/gen/com/android/cb/R.java
@@ -17,12 +17,13 @@ public final class R {
         public static final int button_gray=0x7f020003;
         public static final int button_orange=0x7f020004;
         public static final int ic_launcher=0x7f020005;
-        public static final int img_background_test=0x7f020006;
-        public static final int shape_confirm_dialog=0x7f020007;
-        public static final int shape_confirm_title_background=0x7f020008;
-        public static final int shape_shadow=0x7f020009;
-        public static final int shape_translucent_dialog=0x7f02000a;
-        public static final int spliter=0x7f02000b;
+        public static final int icon_loading=0x7f020006;
+        public static final int img_background_test=0x7f020007;
+        public static final int shape_confirm_dialog=0x7f020008;
+        public static final int shape_confirm_title_background=0x7f020009;
+        public static final int shape_shadow=0x7f02000a;
+        public static final int shape_translucent_dialog=0x7f02000b;
+        public static final int spliter=0x7f02000c;
     }
     public static final class id {
         public static final int botton_OrderList=0x7f070024;
